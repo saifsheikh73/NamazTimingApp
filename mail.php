eaed0cc36d2906dac99ca7cd2f2864c0fd02f16e
saifsheikh73 <saifsheikh73@gmail.com>
2023-06-24 03:06:26 +0530

    spacepush

--- mail.php
+++ mail.php
@@ -36,7 +36,7 @@ try {
 // Content
  $mail->isHTML(true); // Set email format to HTML
  $mail->Subject = 'Here is the subject';
- $mail->Body = 'This is the HTML message body <b>in bold!</b>' . $resetLink;
+ $mail->Body = 'This is the HTML message body <b>in bold!</b> ' . $resetLink;
  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 $mail->send();
