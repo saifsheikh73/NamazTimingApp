c79448b088bc0bc4e57c60a1dda075c30b895012
saifsheikh73 <saifsheikh73@gmail.com>
2023-06-25 02:58:13 +0530

    r13

--- resetpasswordtoken.php
+++ resetpasswordtoken.php
@@ -13,7 +13,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Check if the email exists in the database
         // Perform your database query to check if the email exists
         // Replace the placeholders with your actual database connection code
-        
+        $servername = "localhost"; 
+$username = "questio2_id20710658_db"; 
+$password = "questio2_id20710658_db"; 
+$database = "questio2_namaz_db"; 
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $database); 
+// Check connection 
+if ($conn->connect_error) { 
+die("Connection failed: " . $conn->connect_error); } 
+
+$mailHost = 'mail.questiondrive.com'; // Specify main and backup SMTP servers
+$mailUsername = 'questio2'; // SMTP username
+$mailPassword = 'NamazTiming'; // SMTP password
+$mailSMTPSecure = 'tls'; // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
+$mailPort = 587; // TCP port to connect to
         $query = "SELECT * FROM user1 WHERE email = ?";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $email);
