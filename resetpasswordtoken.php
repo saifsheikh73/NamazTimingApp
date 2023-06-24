1671430d4cfae7caff60df09778ac23ff2d41221
saifsheikh73 <saifsheikh73@gmail.com>
2023-06-25 04:01:32 +0530

    rr

--- resetpasswordtoken.php
+++ resetpasswordtoken.php
@@ -1,6 +1,5 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Process the form data
@@ -48,56 +47,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $stmt = $conn->prepare($query);
             $stmt->bind_param("sss", $resetToken, $expiryTime, $email);
             $stmt->execute();
-            $mail = new PHPMailer(true);
+            
             // Send a password reset email to the user
-            $resetLink = "namaz.questiondrive.com/resetpassword.php?token=" . $resetToken; // Replace with your actual reset password page URL
+            $resetLink = "resetpassword.php?token=" . $resetToken; // Replace with your actual reset password page URL
             $emailContent = "Click the following link to reset your password: " . $resetLink; // Customize the email content as needed
             $emailSubject = "Password Reset";
             // Send the email using your preferred email sending method (e.g., PHPMailer, mail() function)
             // Replace the placeholders with your actual email sending code
-
-            require '/home/questio2/PHPMailerTest/PHPMailer/src/Exception.php';
-require '/home/questio2/PHPMailerTest/PHPMailer/src/PHPMailer.php';
-require '/home/questio2/PHPMailerTest/PHPMailer/src/SMTP.php';
-
-// Instantiation and passing [ICODE]true[/ICODE] enables exceptions
-
-try {
- //Server settings
- $mail->SMTPDebug = 2; // Enable verbose debug output
- $mail->isSMTP(); // Set mailer to use SMTP
- $mail->Host = $mailHost; // Specify main and backup SMTP servers
- $mail->SMTPAuth = true; // Enable SMTP authentication
- $mail->Username = $mailUsername; // SMTP username
- $mail->Password = $mailPassword; // SMTP password
- $mail->SMTPSecure = $mailSMTPSecure; // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
- $mail->Port = $mailPort; // TCP port to connect to
-
-//Recipients
- $mail->setFrom('furqan@namaz.questiondrive.com', 'Mailer');
- $mail->addAddress($email); // Add a recipient
- //$mail->addAddress('sadcompiler@gmail.com'); // Name is optional
- //$mail->addReplyTo('furqan@namaz.questiondrive.com', 'Information');
-//  $mail->addCC('cc@example.com');
-//  $mail->addBCC('bcc@example.com');
-
-// Attachments
-//  $mail->addAttachment('/home/cpanelusername/attachment.txt'); // Add attachments
-//  $mail->addAttachment('/home/cpanelusername/image.jpg', 'new.jpg'); // Optional name
-
-// Content
- $mail->isHTML(true); // Set email format to HTML
- $mail->Subject = 'Here is the subject';
- $mail->Body = 'This is the HTML message body <b>in bold!</b> ' . $emailContent;
- $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-$mail->send();
- echo 'Message has been sent';
-
-} catch (Exception $e) {
- echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-
+            include'mail2.php';
             // Redirect the user to a confirmation page
             header("Location: resetconfirmation.php");  
             exit();
